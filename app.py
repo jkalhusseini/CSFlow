76862d1c8a49359ad08aa91ab2f9d5f0cc340b13
jkalhusseini <jkae2018@mymail.pomona.edu>
2023-09-06 09:06:40 -0700

    Additions to log

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ import time
 
 st.title("CSFlow Computation")
 
+with st.sidebar:
+    options = st.multiselect(
+        'How would you like to analyze the data?',
+        ['PCA','Histogram','3D Scatter', 'PCA Matrix']
+    )
+
 st.subheader("Data Cleaning:")
 uploaded_file = st.file_uploader("Insert CSV File Here!")
 st.caption("After uploading your CSV, ensure the columns are labeled appropriately below.")
@@ -27,6 +33,7 @@ df = pd.read_csv(uploaded_file)
 st.dataframe(df)
 
 
+
 if st.button("Process Data"):
     with st.spinner('Wait for it...'):
         time.sleep(1)
@@ -35,10 +42,10 @@ if st.button("Process Data"):
 
 
 
-    
-     #Histogram
+
     hist_values = px.histogram(
-        df, x = "FR", nbins = 100,
+        df, x = "PC Flow Rate (mL/hr)", 
+        #nbins = 150,
         title = "Histogram of Flow Rate Distributions",
         color = "Etiology",
         #text_auto = True,
@@ -46,11 +53,22 @@ if st.button("Process Data"):
         #x_title = "Flow Rate",
         )
     
-    st.plotly_chart(hist_values)
+    hist_values = px.histogram(
+    df, x = "Etiology", 
+    #nbins = 150,
+    title = "Histogram of Etiology Distributions",
+    color = "PC Flow Rate (mL/hr)",
+    #text_auto = True,
+    #y_title = "Count",
+    #x_title = "Flow Rate",
+    )
     
 
+    st.plotly_chart(hist_values)
+        
+
     SubPlot = make_subplots(rows = 1, cols = 4,
-    y_title= "Flow Rate", x_title="", subplot_titles= ("Age", "BMI", "Months", "FOHR")   
+    y_title= "Flow Rate", x_title="", subplot_titles= ("Age @ Enrollment(y)", "BMI", "Months btwn shunt placement and PC shunt", "FOHR")   
                     )
 
     SubPlot.update_layout(showlegend = False, title = "", plot_bgcolor = 'white')
@@ -58,10 +76,10 @@ if st.button("Process Data"):
     SubPlot.update_xaxes(showline = True, linecolor = 'black', linewidth = 1.5, ticks = "outside", showgrid = True, gridcolor = "lightgray", gridwidth = .5)
     SubPlot.update_yaxes(showline = True, linecolor = 'black', linewidth = 1.5, ticks = "inside", showgrid = True, gridcolor = "lightgray", gridwidth = .5)
 
-    SubPlot.append_trace(go.Scatter(x = df['Age'], y = df["FR"], mode = "markers", marker = dict(color = "red")), row = 1, col = 1)
-    SubPlot.append_trace(go.Scatter(x = df['BMI'], y = df["FR"], mode = "markers", marker = dict(color = "green")), row = 1, col = 2)
-    SubPlot.append_trace(go.Scatter(x = df['Months'], y = df["FR"], mode = "markers", marker = dict(color = "orange")), row = 1, col = 3)
-    SubPlot.append_trace(go.Scatter(x = df['FOHR'], y = df["FR"], mode = "markers", marker = dict(color = "purple")), row = 1, col =4)
+    SubPlot.append_trace(go.Scatter(x = df['Age @ Enrollment(y)'], y = df["PC Flow Rate (mL/hr)"], mode = "markers", marker = dict(color = "red")), row = 1, col = 1)
+    SubPlot.append_trace(go.Scatter(x = df['BMI'], y = df["PC Flow Rate (mL/hr)"], mode = "markers", marker = dict(color = "green")), row = 1, col = 2)
+    SubPlot.append_trace(go.Scatter(x = df['Months btwn shunt placement and PC shunt'], y = df["PC Flow Rate (mL/hr)"], mode = "markers", marker = dict(color = "orange")), row = 1, col = 3)
+    SubPlot.append_trace(go.Scatter(x = df['FOHR'], y = df["PC Flow Rate (mL/hr)"], mode = "markers", marker = dict(color = "purple")), row = 1, col =4)
 
     SubPlot.update_traces(marker = dict(size = 8, line = dict(width = .75, color = "black")))
 
@@ -70,7 +88,7 @@ if st.button("Process Data"):
 
 
    
-    Plot3D = px.scatter_3d(df, x = "FOHR", y = "Age", z = "FR", color = "Etiology")
+    Plot3D = px.scatter_3d(df, x = "FOHR", y = "Age @ Enrollment(y)", z = "PC Flow Rate (mL/hr)", color = "Etiology")
     Plot3D.update_traces(marker_size = 5, opacity = 0.75)
 
     st.plotly_chart(Plot3D)
@@ -78,7 +96,7 @@ if st.button("Process Data"):
 
 
     pca = PCA()
-    features = ["FR", "BMI", "FOHR", "Age", "Months"]
+    features = ["PC Flow Rate (mL/hr)", "BMI", "FOHR", "Age @ Enrollment(y)", "Months btwn shunt placement and PC shunt"]
     components = pca.fit_transform(df[features])
     labels = {
         str(i): f"PC {i+1} ({var:.1f}%)"
